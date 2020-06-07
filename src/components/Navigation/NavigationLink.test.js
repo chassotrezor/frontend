2f1a039af7a2f6946a549c9e03e5ab945b8d0d60
comteharbour <florian.pittet@softozor.ch>
2020-06-07 12:49:27 +0200

    NavigationLink component

---
+++ src/components/Navigation/NavigationLink.test.js
@@ -0,0 +1,80 @@
+import { mountQuasar } from '@test'
+import NavigationLink from './NavigationLink'
+
+describe('NavigationLink', () => {
+  const $router = {
+    push: jest.fn()
+  }
+  const $route = {
+    name: 'currentRoute',
+    params: {}
+  }
+  const wrapper = mountQuasar(NavigationLink, {
+    propsData: {
+      title: 'testTitle',
+      route: {
+        name: 'testRoute'
+      }
+    },
+    mocks: {
+      $router,
+      $route
+    }
+  })
+  const qItem = wrapper.findComponent({ name: 'QItem' })
+
+  it('displays its title', () => {
+    expect(qItem.find('testTiltle')).toBeTruthy()
+  })
+
+  describe('when "route" prop has same name as current route', () => {
+    beforeAll(done => {
+      wrapper.setProps({
+        route: {
+          name: $route.name
+        }
+      })
+      wrapper.vm.$nextTick(done)
+    })
+
+    test('q-item has "active_test" class', () => {
+      expect(qItem.classes()).toContain('active_test')
+    })
+
+    test('q-item is not clickable', () => {
+      expect(qItem.props().clickable).toBe(false)
+    })
+  })
+
+  describe('when "route" prop has different name than current route', () => {
+    beforeAll(done => {
+      wrapper.setProps({
+        route: {
+          name: 'differentName'
+        }
+      })
+      wrapper.vm.$nextTick(done)
+    })
+
+    test('q-item has "inactive_test" class', () => {
+      expect(qItem.classes()).toContain('inactive_test')
+    })
+
+    test('q-item is clickable', () => {
+      expect(qItem.props().clickable).toBe(true)
+    })
+  })
+
+  describe('when "icon" prop is set', () => {
+    beforeAll(done => {
+      wrapper.setProps({
+        icon: 'iconName'
+      })
+      wrapper.vm.$nextTick(done)
+    })
+    it('displays an icon with "icon" name', () => {
+      const qIcon = wrapper.findComponent({ name: 'QIcon' })
+      expect(qIcon.props().name).toBe('iconName')
+    })
+  })
+})
---
+++ src/components/Navigation/NavigationLink.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-item
+    :class="itemClass"
+    :clickable="!isCurrentRoute"
+    @click="$router.push(route)"
+  >
+    <q-item-section
+      v-if="icon"
+      avatar
+    >
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>
+        {{ title }}
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  name: 'NavigationLink',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+
+    route: {
+      type: Object,
+      required: true
+    },
+
+    icon: {
+      type: String,
+      default: ''
+    }
+  },
+
+  computed: {
+    isCurrentRoute () {
+      const sameName = this.$route.name === this.route.name
+      return sameName
+    },
+
+    itemClass () {
+      if (this.isCurrentRoute) return 'bg-secondary text-weight-bolder active_test'
+      else return 'inactive_test'
+    }
+  }
+}
+</script>
