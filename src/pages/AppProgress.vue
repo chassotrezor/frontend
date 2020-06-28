6cf36fd8879b41f3c78511500294d99671c936c7
comteharbour <florian.pittet@softozor.ch>
2020-06-28 15:04:17 +0200

    alpha release

---
+++ src/pages/AppProgress.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="row no-wrap content-center q-ma-xs">
+    <div class="col-3 column justify-center">
+      <div class="text-weight-bold">
+        {{ name }}
+      </div>
+    </div>
+    <q-linear-progress
+      class="col-8"
+      :value="value"
+    />
+    <q-icon
+      v-if="value === 1"
+      class="col-1"
+      name="check"
+      size="md"
+    />
+    <q-icon
+      v-else
+      class="col-1"
+      name="close"
+      size="md"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppProgress',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,32 @@
 <template>
   <q-page class="flex flex-center">
-    Home
+    <div class="column">
+      <div class="text-h6">
+        Cette application web est en version alpha
+      </div>
+      <div>Les parcours créés peuvent être effacés suite à une mise à jour du système.</div>
+      <div style="height: 1em" />
+      <app-progress
+        name="Fonctionnement"
+        :value="1"
+      />
+      <app-progress
+        name="Ergonomie"
+        :value="0.2"
+      />
+      <app-progress
+        name="Esthétique"
+        :value="0"
+      />
+    </div>
   </q-page>
 </template>
 
 <script>
+import AppProgress from './AppProgress'
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: { AppProgress }
 }
 </script>
