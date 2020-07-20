3037f8a8d89f10fdce2fad8a839c9e2fe6f4bb27
comteharbour <florian.pittet@softozor.ch>
2020-07-20 12:03:59 +0200

    rework generateIdIn

--- src/helpers/dataHelpers.js
+++ src/helpers/dataHelpers.js
@@ -8,12 +8,34 @@ export function generateId (length) {
   // return Math.random().toString(36).substring(2)
 }
 
-export function generateIdIn (object, length) {
-  const objectIds = Object.keys(object)
-  const idAlreadyExists = id => objectIds.some(nodeId => id === nodeId)
+function getValue (object, path) {
+  const keys = path ? path.split('.') : null
+  return keys.reduce((actualPosition, key) => actualPosition[key], object)
+}
+
+function idAlreadyExists (id, existingIds) {
+  return existingIds.some(existingId => id === existingId)
+}
+
+export function generateIdIn (objectOrArray, length, pathToValue) {
+  let existingIds
+  switch (typeof objectOrArray) {
+    case 'object': {
+      if (pathToValue) existingIds = Object.values(objectOrArray).map(value => getValue(value, pathToValue))
+      else existingIds = Object.keys(objectOrArray)
+      break
+    }
+    case 'array': {
+      if (pathToValue) existingIds = objectOrArray.map(value => getValue(value, pathToValue))
+      else existingIds = objectOrArray
+      break
+    }
+    default: throw new Error('generateIdIn: can only handle objects or arrays.')
+  }
+
   let newId
   do {
     newId = generateId(length)
-  } while (idAlreadyExists(newId))
+  } while (idAlreadyExists(newId, existingIds))
   return newId
 }
--- src/helpers/dataHelpers.test.js
+++ src/helpers/dataHelpers.test.js
@@ -7,10 +7,41 @@ describe('dataHelpers', () => {
     }
   })
 
-  test('generateIdIn returns a string different of all object keys', () => {
+  test('generateIdIn on object whithout "pathToValue" returns a string different of all object keys', () => {
     const object = {}
     for (let i = 0; i < 100; i++) object[generateId(8)] = ''
     const newKey = generateIdIn(object, 8)
     expect(object).not.toContain(newKey)
+  })
+
+  test('generateIdIn on object with "pathToValue" returns a string different of all values in "object[anyProperty].path.to.value"', () => {
+    const object = {}
+    for (let i = 0; i < 100; i++) {
+      object[generateIdIn(object, 8)] = {
+        path: { to: { value: generateIdIn(object, 8, 'path.to.value') } }
+      }
+    }
+    const newKey = generateIdIn(object, 8, 'path.to.value')
+    expect(object).not.toContain(newKey)
+  })
+
+  test('generateIdIn on array without "pathToValue" returns a string different of all array elements', () => {
+    const array = []
+    for (let i = 0; i < 100; i++) {
+      array[i] = generateIdIn(array, 8)
+    }
+    const newKey = generateIdIn(array, 8)
+    expect(array).not.toContain(newKey)
+  })
+
+  test('generateIdIn on array with "pathToValue" returns a string different of all values in "array[anyElement].path.to.value"', () => {
+    const array = []
+    for (let i = 0; i < 100; i++) {
+      array[i] = {
+        path: { to: { value: generateIdIn(array, 8, 'path.to.value') } }
+      }
+    }
+    const newKey = generateIdIn(array, 8, 'path.to.value')
+    expect(array).not.toContain(newKey)
   })
 })
